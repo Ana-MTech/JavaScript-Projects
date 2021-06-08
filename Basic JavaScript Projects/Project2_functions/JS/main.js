6e189b371d23d6083f907f61dd00bbbac4349436
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2021-06-08 23:23:38 +0100

    Add files via upload

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,30 +1,7 @@
-document.write ("Hello, World!");
-
-var A = "This is a string";
-document.write(A)
-
-var A = "This is a string";
-window.alert(A)
-
-document.write("Lisa told Bart:\"Knock it off! Or I tell dad!\""+"-Lisa Simpson");
-
-var B = "Lisa told Bart:\"Knock it off! Or I tell dad!\""+"-Lisa Simpson" + "This is a string"
-document.write(B)
-
-var Family = "The Browns", Dad = "Mark", Mom = "Andrea", Older_sun = "Tony", Younger_sun = "Matt";
-document.write(Mom);
-
-var A = "This is a string";
-var A = A.fontcolor ("red")
-document.write(A)
-
-var B = "Lisa told Bart:\"Knock it off! Or I tell dad!\""+"-Lisa Simpson" + "This is a string"
-var B = B.fontcolor ("green")
-document.write(B)
-
-document.write(3+2)
-
-function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
+function myFunction() {
+    var text1 = "Hello"; 
+    var text2 = "World!"; 
+    text1 += text2 // The += operator can also be used to concatenate (add) strings
+    document.getElementById("demo").innerHTML = text1; // text1 has been add to text2 and display
   }
 
