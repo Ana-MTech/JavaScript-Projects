6f6dab7e9dedb0a7f7156e215baeae115315cd8b
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2021-06-09 00:11:49 +0100

    Add files via upload

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,14 +1,5 @@
-var booking = "Book \"now\"!"; //Defining wariable and giving it string value
-window.alert(booking)
-
-var aptName = "\"Maria\", see view apartment.";
-var aptName = aptName.fontcolor ("red"); //Using fontcolor method on string variable
-document.write(aptName)
-
-document.write("Book \"now\"!"+"\"Maria\", see view apartment."); // Concatenate string
-
 
 var x = 5;    // Declare x, give it the value of 5
-var y = x + 4;  // Declare y, give it the value of x + 2 
-// Write y to demo:
-document.getElementById("demo").innerHTML = y;
+var y = 4;  // Declare y, give it the value of x + 2 
+var z = x +y;
+document.getElementById("demo").innerHTML = +z;
