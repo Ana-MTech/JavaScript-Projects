1c32b7d4cc55c5985b6fc454ce479fe630f61c1a
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2021-06-08 18:20:52 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,30 @@
+document.write ("Hello, World!");
+
+var A = "This is a string";
+document.write(A)
+
+var A = "This is a string";
+window.alert(A)
+
+document.write("Lisa told Bart:\"Knock it off! Or I tell dad!\""+"-Lisa Simpson");
+
+var B = "Lisa told Bart:\"Knock it off! Or I tell dad!\""+"-Lisa Simpson" + "This is a string"
+document.write(B)
+
+var Family = "The Browns", Dad = "Mark", Mom = "Andrea", Older_sun = "Tony", Younger_sun = "Matt";
+document.write(Mom);
+
+var A = "This is a string";
+var A = A.fontcolor ("red")
+document.write(A)
+
+var B = "Lisa told Bart:\"Knock it off! Or I tell dad!\""+"-Lisa Simpson" + "This is a string"
+var B = B.fontcolor ("green")
+document.write(B)
+
+document.write(3+2)
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+  }
+
