d52f5427ec6c9388ce3c39fc433bb6324dbb530f
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-02-09 22:46:13 +0000

    Add files via upload

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -4,7 +4,19 @@ function my_Dictionary() {
         Surname:"Cahill",
         Year: 2002,
         Class:"Second",
-        Subject: "Computer sciance",
+        Subject:"Computer sciance",
     };
     document.getElementById("Dictionary").innerHTML = Student.Surname;
+}
+
+function my_Dictionary1() {
+    var Student = {
+        Name:"Mark",
+        Surname:"Cahill",
+        Year: 2002,
+        Class:"Second",
+        Subject:"Computer sciance",
+    };
+    delete Student.Surname;
+    document.getElementById("Dictionary1").innerHTML = Student.Surname;
 }
