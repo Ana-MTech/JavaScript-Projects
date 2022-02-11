8b77e2a622ec1b5c6deca90886d9184babddfdcd
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-02-11 16:33:20 +0000

    Add files via upload

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,6 @@
+    function get_Date() {
+        if (new Date().getHours() > 10)  {
+        document.getElementById("My_Function") .innerHTML = "Time to rest. Good night!";
+        }
+    }
+    
